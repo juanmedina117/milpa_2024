228c64de0f6a3cdbffc1fb595362007336735cdf
juanmedina117 <47828097+juanmedina117@users.noreply.github.com>
2024-06-02 10:43:08 -0600

    Update movilizadorController.js

--- backend/controllers/movilizadorController.js
+++ backend/controllers/movilizadorController.js
@@ -1,63 +1,52 @@
 import { check, validationResult } from "express-validator";
 
-import Conteo from '../models/Conteo.js';
+import Movilizador from '../models/Movilizador.js';
 
 
 
-const registrarConteo = async (req, res) => {
+const registrarMovilizador = async (req, res) => {
 
-    try {
-        
+    console.log(req.body);
 
+    // Validacion
+    await check('nombre').notEmpty().run(req);
+    await check('numero_movilizador').notEmpty().run(req);
+    await check('seccion').notEmpty().run(req);
+    await check('latitud').notEmpty().run(req);
+    await check('longitud').notEmpty().run(req);
 
-        console.log(req.body);
-    
-        // Validacion
-        await check('numero_conteo').notEmpty().run(req);
-        await check('seccion').notEmpty().run(req);
-        await check('latitud').notEmpty().run(req);
-        await check('longitud').notEmpty().run(req);
-    
-        let resultado = await validationResult(req);
-    
-        if( !resultado.isEmpty() ){
-            res.json({
-                code:0,
-                msg:"EL campo esta vacio"
-            })
-        }
-    
-        const conteo = await Conteo.create(req.body);
-    
-        
-        if(conteo){
-    
-           return  res.status(200).send({
-                code:1,
-                msg:"Registro realizado con exito.",
-                conteo
-            });
-            
-        }else{
-          return  res.status(500).send({
-                code:0,
-                msg:"NO se guardo el registro",
-                conteo
-            });
-            
-        }
-        
-    } catch (error) {
-        console.log(error);
+    let resultado = await validationResult(req);
+
+    if( !resultado.isEmpty() ){
+        res.json({
+            code:0,
+            msg:"EL campo esta vacio"
+        })
     }
 
+    const movilizador = await Movilizador.create(req.body);
+    if(movilizador){
 
-
+        res.json({
+            code:1,
+            msg:"Registro realizado con exito.",
+            movilizador
+        });
+        
+    }else{
+        res.json({
+            code:0,
+            msg:"NO se guardo el registro",
+            conteo
+        });
+        
+    }
+    
 
 
 
 }
 
 export {
-    registrarConteo
+    registrarMovilizador
 }
