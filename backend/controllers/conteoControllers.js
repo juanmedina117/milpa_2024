beb8fcf367a109ae76564f83cd55e63e5d9a80e0
juanmedina117 <47828097+juanmedina117@users.noreply.github.com>
2024-06-02 10:42:23 -0600

    Update conteoControllers.js

--- backend/controllers/conteoControllers.js
+++ backend/controllers/conteoControllers.js
@@ -1,58 +1,38 @@
-import { check, validationResult } from "express-validator";
-
-import Conteo from '../models/Conteo.js';
 
 
+    // Validacion
+    await check('numero_conteo').notEmpty().run(req);
+    await check('seccion').notEmpty().run(req);
+    await check('latitud').notEmpty().run(req);
+    await check('longitud').notEmpty().run(req);
 
-const registrarConteo = async (req, res) => {
+    let resultado = await validationResult(req);
 
-    try {
-        
-
-
-        console.log(req.body);
-    
-        // Validacion
-        await check('numero_conteo').notEmpty().run(req);
-        await check('seccion').notEmpty().run(req);
-        await check('latitud').notEmpty().run(req);
-        await check('longitud').notEmpty().run(req);
-    
-        let resultado = await validationResult(req);
-    
-        if( !resultado.isEmpty() ){
-            res.json({
-                code:0,
-                msg:"EL campo esta vacio"
-            })
-        }
-    
-        const conteo = await Conteo.create(req.body);
-    
-        
-        if(conteo){
-    
-           return  res.status(200).send({
-                code:1,
-                msg:"Registro realizado con exito.",
-                conteo
-            });
-            
-        }else{
-          return  res.status(500).send({
-                code:0,
-                msg:"NO se guardo el registro",
-                conteo
-            });
-            
-        }
-        
-    } catch (error) {
-        console.log(error);
+    if( !resultado.isEmpty() ){
+        res.json({
+            code:0,
+            msg:"EL campo esta vacio"
+        })
     }
 
+    const conteo = await Conteo.create(req.body);
+    if(conteo){
 
-
+        res.json({
+            code:1,
+            msg:"Registro realizado con exito.",
+            conteo
+        });
+        
+    }else{
+        res.json({
+            code:0,
+            msg:"NO se guardo el registro",
+            conteo
+        });
+        
+    }
+    
 
 
 
